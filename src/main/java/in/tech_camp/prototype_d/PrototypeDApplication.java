b5d09aff594f81f551a985c1ffec21fad094bd25
Y.takahashi <Y.takahashi@infomart.co.jp>
2026-07-17 10:45:56 +0900

    first commit

---
+++ src/main/java/in/tech_camp/prototype_d/PrototypeDApplication.java
@@ -0,0 +1,13 @@
+package in.tech_camp.prototype_d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeDApplication.class, args);
+	}
+
+}

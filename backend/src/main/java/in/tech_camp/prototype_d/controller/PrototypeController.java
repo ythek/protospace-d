c2b9cc8e25c57fbec911930322ea428c496c421e
tskiri <t.kiribayashi@infomart.co.jp>
2026-07-22 10:44:21 +0900

    DTOの追加

--- backend/src/main/java/in/tech_camp/prototype_d/controller/PrototypeController.java
+++ backend/src/main/java/in/tech_camp/prototype_d/controller/PrototypeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import in.tech_camp.prototype_d.entity.PrototypeEntity;
+import in.tech_camp.prototype_d.dto.PrototypeDto;
 import in.tech_camp.prototype_d.service.PrototypeService;
 import lombok.RequiredArgsConstructor;
 
@@ -20,10 +20,10 @@ public class PrototypeController {
 
   private final PrototypeService prototypeService;
 
-  @GetMapping("/")
+  @GetMapping({"/prototypes", "/", ""})
   public ResponseEntity<?> getPrototypes() {
     try {
-      List<PrototypeEntity> prototypes = prototypeService.getPrototypes();
+      List<PrototypeDto> prototypes = prototypeService.getPrototypes();
       return ResponseEntity.ok().body(prototypes);
     } catch (Exception e) {
       e.printStackTrace(); // エラーあったら見たい

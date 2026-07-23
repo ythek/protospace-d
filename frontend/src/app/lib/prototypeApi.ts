e4effe3220596c6026ed3c84a76abdd1e8c27c8b
tskiri <t.kiribayashi@infomart.co.jp>
2026-07-23 11:50:11 +0900

    エンドポイントの設定

--- frontend/src/app/lib/prototypeApi.ts
+++ frontend/src/app/lib/prototypeApi.ts
@@ -1,7 +1,28 @@
 import apiClient from './apiClient';
 import { PrototypeData } from './prototypeData';
+import { CommentData } from './commentData';
 
 export const fetchPrototypes = async (): Promise<PrototypeData[]> => {
   const response = await apiClient.get('/api/prototypes');
+  return response.data;
+};
+
+// 詳細画面取得（担当外なのでコメントアウトしておきます）
+// export const fetchByPrototypeId = async (prototypeId: string ): Promise<PrototypeData[]> => {
+//   const response = await apiClient.get(`/api/prototypes/${prototypeId}`);
+//   return response.data;
+// }
+
+// コメント一覧取得
+export const fetchComments = async (prototypeId: string ): Promise<CommentData[]> => {
+  const response = await apiClient.get<CommentData[]>(`/api/prototypes/${prototypeId}/comments`);
+  return response.data;
+};
+
+// コメント投稿
+export const createComment = async (prototypeId: string , comment: string) => {
+  const response = await apiClient.post(`/api/prototypes/${prototypeId}/comments`, {
+    comment,
+  });
   return response.data;
 };

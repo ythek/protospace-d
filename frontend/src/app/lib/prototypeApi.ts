add587e6a18cae0abddca0f8ddcd40319ba486fd
ythek <y.takahashi@infomart.co.jp>
2026-07-23 17:15:24 +0900

    詳細機能　レビュー後修正
    APIをたたけていなかったのでそこの修正。

--- frontend/src/app/lib/prototypeApi.ts
+++ frontend/src/app/lib/prototypeApi.ts
@@ -1,12 +1,13 @@
 import apiClient from './apiClient';
-import { PrototypeData } from './prototypeData';
+//import { PrototypeData } from './prototypeData';
+import { PrototypeData } from "@/app/components/PrototypeDetail";
 
 export const fetchPrototypes = async (): Promise<PrototypeData[]> => {
   const response = await apiClient.get('/api/prototypes');
   return response.data;
 };
-export const fetchPrototypeById = async (): Promise<PrototypeData[]> => {
-  const response = await apiClient.get('/api/prototypes/${prototypeId}');
-  return response.data
 
-}
+export const fetchPrototypeById = async (prototypeId: number | string): Promise<PrototypeData> => {
+  const response = await apiClient.get(`/api/prototypes/${prototypeId}`);
+  return response.data;
+};

522637490e766fc7cd31e95f126a8b528c455542
tskiri <t.kiribayashi@infomart.co.jp>
2026-07-24 13:45:06 +0900

    Controller

--- backend/src/main/java/in/tech_camp/prototype_d/controller/PrototypeController.java
+++ backend/src/main/java/in/tech_camp/prototype_d/controller/PrototypeController.java
@@ -1,6 +1,7 @@
 package in.tech_camp.prototype_d.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,7 @@ public class PrototypeController {
 
   private final PrototypeRepository prototypeRepository;
 
+  // プロトタイプ一覧表示
   @GetMapping({"/prototypes", "/", ""})
   public ResponseEntity<?> getPrototypes() {
     try {
@@ -37,19 +39,33 @@ public class PrototypeController {
     }
   }
 
+  // プロトタイプ詳細表示
   @GetMapping("/prototypes/{prototypeId}")
   public ResponseEntity<?> showPrototypeDetail(@PathVariable("prototypeId") Integer prototypeId) {
-  try {
-    PrototypeEntity prototype = prototypeRepository.findById(prototypeId);
-    if(prototype == null){
-      return ResponseEntity.notFound().build(); 
+    try {
+      PrototypeEntity prototype = prototypeRepository.findById(prototypeId);
+      if(prototype == null){
+        return ResponseEntity.notFound().build(); 
+      }
+        return ResponseEntity.ok().body(prototype);
+      } catch (Exception e) {
+        System.out.println("エラー :" + e);
+    
+      return ResponseEntity.internalServerError().body("サーバーエラー");
     }
-      return ResponseEntity.ok().body(prototype);
+  }
+
+  // プロトタイプ削除機能
+  @DeleteMapping("/prototypes/${prototypeId}/delete")
+  public ResponseEntity<?> deletePrototype(@PathVariable("prototypeId") Long prototypeId) {
+    try {
+      prototypeService.deletePrototype(prototypeId);
+      return ResponseEntity.ok().body(Map.of(
+        "Message", "プロトタイプを削除しました", "deletePrototypeId", prototypeId
+      ));
     } catch (Exception e) {
-      System.out.println("エラー :" + e);
-  
-    return ResponseEntity.internalServerError().body("サーバーエラー");
-      
-}
-}
+      e.printStackTrace();
+      return ResponseEntity.internalServerError().body(Map.of("messages", List.of("プロトタイプの削除に失敗しました")));
+    }
+  }
 }

8de0e9a6f1097dba2888e9c36ab30e498deff931
ythek <y.takahashi@infomart.co.jp>
2026-07-30 10:09:36 +0900

    フロントエンドのパス修正２

--- backend/src/main/java/in/tech_camp/prototype_d/config/SecurityConfig.java
+++ backend/src/main/java/in/tech_camp/prototype_d/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", frontendUrl));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);

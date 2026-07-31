6879d844ae4596ba97f99fd83f1b995961edc394
hikaru-stannum <h.suzue@infomart.co.jp>
2026-07-31 10:30:07 +0900

    新規投稿フロント ログイン状態分岐

--- frontend/src/app/prototypes/new/page.tsx
+++ frontend/src/app/prototypes/new/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
 import PrototypeForm from '@/components/PrototypeForm';
 import { createPrototype, PrototypeFormData } from '@/lib/prototypeApi';
+import { useAuthContext } from '@/app/context/AuthContext';
 const CreatePrototypePage = () => {
   const router = useRouter();
   const [errorMessages, setErrorMessages] = useState<string[]>([]);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const {user, isLoading} = useAuthContext();
 
   const initialFormData: PrototypeFormData = {
     title: '',
@@ -47,7 +49,9 @@ const CreatePrototypePage = () => {
 
     }
   };
-
+  if(isLoading) return <div>読み込み中</div>
+  if(!user)  return redirect('/users/sign_in');
+  
   return (
     <div className="contents row">
       <div className="container">

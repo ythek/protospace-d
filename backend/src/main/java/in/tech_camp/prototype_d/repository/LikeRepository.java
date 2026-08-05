c9c2ea446cc008af899e3d3ea42b79f5a5b60fe6
ythek <y.takahashi@infomart.co.jp>
2026-08-05 16:38:00 +0900

    LikeRepository作成

---
+++ backend/src/main/java/in/tech_camp/prototype_d/repository/LikeRepository.java
@@ -0,0 +1,44 @@
+package in.tech_camp.prototype_d.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import in.tech_camp.prototype_d.dto.PrototypeListDto;
+
+@Mapper
+public interface LikeRepository {
+  //いいね追加
+  @Insert("INSERT INTO likes (user_id, prototype_id)" +
+          "VALUES (#{userId}, #{prototypeId})")
+  void insertLikes(@Param("userId") Long userId,
+                   @Param("prototypeId") Long prototypeId);
+
+  //いいね削除
+  @Delete("DELETE FROM likes WHERE user_id = #{userId} AND prototype_id = #{prototypeId}")
+  void deleteLikes(@Param("userId") Long userId,
+                   @Param("prototypeId") Long prototypeId);
+  //いいねされているか判定 
+  @Select("SELECT COUNT(*) FROM likes WHERE user_id = #{userId} AND prototype_id = #{prototypeId}")
+   boolean existLikes(@Param("userId") Long userId,
+                      @Param("prototypeId") Long prototypeId);
+
+  //いいね総数取得
+  @Select("SELECT COUNT(*) FROM likes WHERE prototype_id = #{prototypeId}" ) //自分がいいね何回されたかを知りたいためUserIdは不要
+  long countAllLikes(Long prototypeId);
+
+  //いいねが多い順
+  @Select("""
+          SELECT p.title, u.username, p.catchcopy, p.image  COUNT(l.id) AS likecount
+          FROM prototypes p  
+          JOIN users ON p.user_id = u.id
+          LEFT JOIN likes l ON p.id = l.prototype_id
+          GROUP BY p.id
+          ORDER BY likeCount DESC
+          """)
+  List<PrototypeListDto> orderByLikes ();
+}
